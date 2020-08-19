f6a948f31fdb7f146781cb3b6ec1cda5032d02a6
yangzhi321 <1021936370@qq.com>
2020-08-20 07:10:00 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+   
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+import axios from "axios"
+axios.get(
+ "http://123.207.32.32:8000/home/multidata"
+).then(res =>{console.log(res);})
+
+</script>
+<style>
+
+</style>
